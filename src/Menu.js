967599c4e091158e9ac8afb677bd024f2151f074
excelitai-mahedi <mahedi@excelitai.com>
2022-02-03 15:43:19 +0600

    first

---
+++ src/Menu.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <div>
+        <aside className="main-sidebar sidebar-dark-primary elevation-4">
+          {/* Brand Logo */}
+          <a className="brand-link">
+            <img
+              src="dist/img/AdminLTELogo.png"
+              alt="AdminLTE Logo"
+              className="brand-image img-circle elevation-3"
+              style={{ opacity: ".8" }}
+            />
+            <span className="brand-text font-weight-light">OMUK COmpany</span>
+          </a>
+          {/* Sidebar */}
+          <div className="sidebar">
+            {/* Sidebar user panel (optional) */}
+            <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+              <div className="image">
+                <img
+                  src="dist/img/user2-160x160.jpg"
+                  className="img-circle elevation-2"
+                  alt="User Image"
+                />
+              </div>
+              <div className="info">
+                <a href="#" className="d-block">
+                  Alexander Pierce
+                </a>
+              </div>
+            </div>
+            {/* Sidebar Menu */}
+            <nav className="mt-2">
+              <ul
+                className="nav nav-pills nav-sidebar flex-column"
+                data-widget="treeview"
+                role="menu"
+                data-accordion="false"
+              >
+                {/* Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library */}
+
+                <li className="nav-item">
+                  <a  className="nav-link">
+                    <i className="nav-icon fas fa-th" />
+                    <p>
+                      Widgets
+                      <span className="right badge badge-danger">New</span>
+                    </p>
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link">
+                    <i className="nav-icon far fa-image" />
+                    <p>Gallery</p>
+                  </a>
+                </li>
+                <li className="nav-item has-treeview">
+                  <a className="nav-link">
+                    <i className="nav-icon far fa-envelope" />
+                    <p>
+                      Mailbox
+                      <i className="fas fa-angle-left right" />
+                    </p>
+                  </a>
+                  <ul className="nav nav-treeview">
+                    <li className="nav-item">
+                      <a className="nav-link">
+                        <i className="far fa-circle nav-icon" />
+                        <p>Inbox</p>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+            {/* /.sidebar-menu */}
+          </div>
+          {/* /.sidebar */}
+        </aside>
+      </div>
+    );
+  }
+}
